8e8eb1907638875d223ff2b0321c8033b0b317ef
Buer <wschenbuer@yahoo.com>
2019-03-13 21:05:58 +0100

    Update HeadCount.py

--- backend/HeadCount.py
+++ backend/HeadCount.py
@@ -2,7 +2,7 @@ import mysql.connector
 import pytest
 import unittest
 
-mydb = mysql.connector.connect(user='root', password='huawei123', host='127.0.0.1', database='pythonDB')
+mydb = mysql.connector.connect(user='root', password='huawei123', host='localhost', database='pythonDB')
 # mydb = mysql.connector.connect(user='chen', password='chen123', host='localhost', database='pythonDB')
 cursor = mydb.cursor()
 # cursor.execute("CREATE TABLE points (XCoordinate INT(5), YCoordinate INT(5))")
